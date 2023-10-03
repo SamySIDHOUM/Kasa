100ed350d86a249f8b10e4eac3c5a5a909514fc4
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-10-03 10:09:12 +0200

    Importation du style et suppression de la div en trop

--- src/pages/Housings.jsx
+++ src/pages/Housings.jsx
@@ -5,6 +5,7 @@ import Carrousel from '../components/Carrousel';
 import Housing from '../components/Housing';
 import Collapse from '../components/Collapse';
 import Error from './Error';
+import "../styles/_housings.scss";
 
 export default function Housings() {
   const params = useParams();
@@ -28,32 +29,30 @@ export default function Housings() {
   };
 
   return (
-    <div>
-      <div className="container">
-        <main>
-          <Carrousel pictures={housingDetails.pictures} />
-          <Housing
-            id={housingDetails.id}
-            title={housingDetails.title}
-            location={housingDetails.location}
-            tags={housingDetails.tags}
-            host={housingDetails.host}
-            rating={housingDetails.rating}
-          />
-          <div className="housing-collapse">
-            <Collapse title="Description">
-              {housing.description}
-            </Collapse>
-            <Collapse title="Équipements">
-              <ul>
-                {housing.equipments.map((equipment, id) => (
-                  <li key={id}>{equipment}</li>
-                ))}
-              </ul>
-            </Collapse>
-          </div>
-        </main>
-      </div>
+    <div className="container">
+      <main>
+        <Carrousel pictures={housingDetails.pictures} />
+        <Housing
+          id={housingDetails.id}
+          title={housingDetails.title}
+          location={housingDetails.location}
+          tags={housingDetails.tags}
+          host={housingDetails.host}
+          rating={housingDetails.rating}
+        />
+        <div className="housing-collapse">
+          <Collapse title="Description">
+            {housing.description}
+          </Collapse>
+          <Collapse title="Équipements">
+            <ul>
+              {housing.equipments.map((equipment, id) => (
+                <li key={id}>{equipment}</li>
+              ))}
+            </ul>
+          </Collapse> 
+        </div>
+      </main>
     </div>
   );
 }
